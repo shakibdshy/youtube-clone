13e4ff82d43149065d2f86d849846614f5250724
Md Habibur Rahman <shakibdshy@gmail.com>
2022-07-23 22:23:23 +0600

    Header Section Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,94 @@
 import Image from 'next/image'
 import React from 'react'
 import logo from '../assets/logo.png'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { FiAlertCircle, FiSearch } from 'react-icons/fi'
-import { Input, Tooltip } from '@mantine/core'
+import { BiVideoPlus, BiBell } from 'react-icons/bi'
+import { RiVideoLine } from 'react-icons/ri'
+import { CgLivePhoto } from 'react-icons/cg'
+import { Avatar, createStyles, Header, Autocomplete, Group, Burger, Menu, Tooltip, ActionIcon } from '@mantine/core';
+import { useBooleanToggle } from '@mantine/hooks';
+import { Search } from 'tabler-icons-react';
 
-function Header() {
-    const rightSection = (
-        <Tooltip label="We do not send spam" position="top" placement="end">
-            <FiSearch size={24} style={{ display: 'block' }} />
-        </Tooltip>
-    );
+const useStyles = createStyles((theme) => ({
+    header: {
+        paddingLeft: theme.spacing.md,
+        paddingRight: theme.spacing.md,
+    },
+
+    inner: {
+        height: 56,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    search: {
+        [theme.fn.smallerThan('sm')]: {
+            display: 'none',
+        },
+        flex: '0 0 auto',
+        width: '100%',
+        maxWidth: '600px',
+    },
+
+    social: {
+        width: 260,
+
+        [theme.fn.smallerThan('sm')]: {
+            width: 'auto',
+            marginLeft: 'auto',
+        },
+    },
+}));
+
+
+export function HeaderSearch() {
+    const [opened, toggleOpened] = useBooleanToggle(false);
+    const { classes } = useStyles();
+
     return (
-        <>
-            <section>
-                <div>
-                    <GiHamburgerMenu />
+        <Header height={56} className={classes.header} mb={120}>
+            <div className={classes.inner}>
+                <Group>
+                    <Burger opened={opened} onClick={() => toggleOpened()} size="sm" />
                     <Image src={logo} alt="logo" width={120} height={26} />
-                </div>
-                <div>
-                    <form style={{ marginLeft: '20px', marginRight: '20px' }}>
-                        <Input placeholder="Your email" rightSection={rightSection} classNames={{ rightSection: 'search-icon'}} />
-                    </form>
-                </div>
-                <div>
+                </Group>
 
-                </div>
-            </section>
-        </>
-    )
+                <Autocomplete
+                    className={classes.search}
+                    placeholder="Search"
+                    icon={<Search size={16} />}
+                    data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
+                />
+
+                <Group spacing={20} className={classes.social} position="right" noWrap>
+                    <Menu control={
+                        <Tooltip label="Create" position="bottom" placement="end">
+                            <ActionIcon size='xl' radius='xl'>
+                                <BiVideoPlus size={20} />
+                            </ActionIcon>
+                        </Tooltip>
+                    }>
+                        <Menu.Item icon={<RiVideoLine size={14} />}>Settings</Menu.Item>
+                        <Menu.Item icon={<CgLivePhoto size={14} />}>Messages</Menu.Item>
+                    </Menu>
+                    <Menu control={
+                        <Tooltip label="Notifications" position="bottom" placement="end">
+                            <ActionIcon size='xl' radius='xl'>
+                                <BiBell size={20} />
+                            </ActionIcon>
+                        </Tooltip>
+                    }>
+                        <Menu.Item icon={<RiVideoLine size={14} />}>Settings</Menu.Item>
+                        <Menu.Item icon={<CgLivePhoto size={14} />}>Messages</Menu.Item>
+                    </Menu>
+                    <Menu control={
+                        <Avatar src={null} alt="no image here" size='md' radius='xl' />
+                    }>
+                        <Menu.Item icon={<RiVideoLine size={14} />}>Settings</Menu.Item>
+                        <Menu.Item icon={<CgLivePhoto size={14} />}>Messages</Menu.Item>
+                    </Menu>
+                </Group>
+            </div>
+        </Header>
+    );
 }
-
-export default Header
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,11 @@
 import React, { PropsWithChildren, ReactElement } from 'react'
-import Header from './Header'
+import { HeaderSearch } from './Header';
 
 export default function Layout({ children }: PropsWithChildren<{}>) {
     return (
         <>
             <div className="body-container flex">
-                <Header />
+                <HeaderSearch />
                 <main className='w-full sm:w-[95%] sm:ml-[105px]'>
                     {children}
                 </main>
